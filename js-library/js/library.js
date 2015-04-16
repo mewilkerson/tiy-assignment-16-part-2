a242983441ec4434d360878347c9af91b7dbe1cd
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-04-16 17:18:46 -0400

    changed syntax

--- js-library/js/library.js
+++ js-library/js/library.js
@@ -1,45 +1,45 @@
 
 var veryFancyMathLibrary = {
 
-  add = function (num1, num2) {
+  add: function (num1, num2) {
     return num1 + num2;
   },
 
-  subtract = function (num1, num2) {
+  subtract: function (num1, num2) {
     return num2 - num1;
   },
 
-  divide = function (num1, num2) {
+  divide: function (num1, num2) {
     return num1 / num2;
   },
 
-  multiply = function (num1, num2) {
+  multiply: function (num1, num2) {
     return num1 * num2;
   },
 
-  increment = function (num) {
+  increment: function (num) {
     return num++;
   },
 
-  decrement = function (num) {
+  decrement: function (num) {
     return num--;
   },
 
-  remainder = function (num1, num2) {
+  remainder: function (num1, num2) {
     return num1 % num2;
   },
 
-  sumAndDouble = function (num1, num2) {
+  sumAndDouble: function (num1, num2) {
     var sum = num1 + num2;
     return sum * 2;
   },
 
-  divideAndTriple = function (num1, num2) {
+  divideAndTriple: function (num1, num2) {
     var quotient = num1 / num2;
     return quotient * 3;
   },
 
-  cubeMeBaby = function (num) {
+  cubeMeBaby: function (num) {
     return num * num * num;
   }
 
